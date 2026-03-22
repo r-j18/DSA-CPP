d24aa259109135a3c49486ba3241acb86ab05a36
Romit Jadhav <romitj1806@gmail.com>
2026-03-22 18:36:14 +0530

    Problem 5 comments added

--- Strivers Practice/Basics/Day_2/Pattern_5.cpp
+++ Strivers Practice/Basics/Day_2/Pattern_5.cpp
@@ -5,7 +5,9 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
+    //Outer loop runs 5 times but i decreases with every iteration
     for(int i = 5; i>0; i--){
+        //Inner loop runs 5 times then 4 then 3 ... then once
         for(int j = 0; j< i; j++){
             cout << "*";
         }
