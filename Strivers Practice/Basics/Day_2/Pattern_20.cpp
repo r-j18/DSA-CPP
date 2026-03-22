f6bfce10e63dbb979a96773e3e073055d6bf9d87
Romit Jadhav <romitj1806@gmail.com>
2026-03-22 20:18:26 +0530

    Pattern 20

---
+++ Strivers Practice/Basics/Day_2/Pattern_20.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //Outer loop runs 5 times
+    for(int i = 0; i<5 ; i++){
+        // Inner loop runs first 1 then 2 then 3 .... then 5 times
+        for(int j = 0; j<= i; j++){
+            cout << "*";    
+        }
+        for(int j = 0; j< 8- 2*i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j<= i; j++){
+            cout << "*";    
+        }
+        cout << "\n";
+    }
+    for(int i = 0; i<5 ; i++){
+        // Inner loop runs first 1 then 2 then 3 .... then 5 times
+        for(int j = 4; j>= i; j--){
+            cout << "*";    
+        }
+        for(int j = 0; j< 2*i; j++){
+            cout << " ";
+        }
+        for(int j = 4; j>= i; j--){
+            cout << "*";    
+        }
+        cout << "\n";
+    }
+
+
+    return 0;
+}
