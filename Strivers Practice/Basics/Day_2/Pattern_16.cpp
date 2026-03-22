e4e7c4dcbdda8daf0a7652d0b4a76d6ded2068e7
Romit Jadhav <romitj1806@gmail.com>
2026-03-22 18:53:57 +0530

    Pattern 16

---
+++ Strivers Practice/Basics/Day_2/Pattern_16.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x =65;
+    //Outer loop runs 5 times
+    for(int i = 0; i<5 ; i++){
+        
+        // Inner loop runs first 1 then 2 then 3 .... then 5 times
+        for(int j = 0; j<= i; j++){
+            cout << (char) x ;
+            
+        }
+        x++;
+        cout << "\n";
+    }
+
+    return 0;
+}
