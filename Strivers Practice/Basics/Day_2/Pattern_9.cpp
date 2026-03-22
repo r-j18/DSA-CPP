b71d6a988dd11bcd8498ec401e8e9251335bc313
Romit Jadhav <romitj1806@gmail.com>
2026-03-22 17:07:19 +0530

    Solved 9

---
+++ Strivers Practice/Basics/Day_2/Pattern_9.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    //Outer loop runs 5 times i increases with every iteration
+    for(int i = 0; i < 5 ; i++){
+        //This loop makes first 4 spaces then 3 then 2 ....
+        for(int j = 0; j < 5-i-1 ; j= j+1){
+            cout << " ";
+        }
+        //This Loop prints stars first 1 then 2 then ....
+        for(int j = 0; j < 2*i + 1 ; j= j+1){
+            cout << "*";
+        }
+        //This loop makes first 4 spaces then 3 then 2 ....
+        for(int j = 0; j < 5-i-1 ; j= j+1){
+            cout << " ";
+        }
+        cout << "\n";
+    }
+    //Outer loop runs 5 times i increases with every iteration
+    for(int i = 4; i >= 0 ; i--){
+        //This loop starts by printing 0 spaces then increases to 4
+        for(int j = 5-i-1; j > 0 ; j= j-1){
+            cout << " ";
+        }
+        //This loop first prints 9 then .... 1 star
+        for(int j = 0; j < 2*i +1 ; j= j+1){
+            cout << "*";
+        }
+        //This loop starts by printing 0 spaces then increases to 4
+        for(int j = 5-i-1; j > 0 ; j= j-1){
+            cout << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
