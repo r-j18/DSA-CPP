b15dec46b4f9ff4f2fd5b02cb7e68b63311537ac
Romit Jadhav <romitj1806@gmail.com>
2026-03-22 17:03:00 +0530

    Pattern 8 solved

---
+++ Strivers Practice/Basics/Day_2/Pattern_8.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    //Outer loop runs 5 times i increases with every iteration
+    for(int i = 4; i >= 0 ; i--){
+        //This loop starts by printing 0 spaces then increases to 4
+        for(int j = 5-i-1; j > 0 ; j= j-1){
+            cout << " ";
+        }
+        //This loop first prints 9 then .... 1 star
+        for(int j = 0; j < 2*i +1 ; j= j+1){
+            cout << "*";
+        }
+        //This loop starts by printing 0 spaces then increases to 4
+        for(int j = 5-i-1; j > 0 ; j= j-1){
+            cout << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
