edf7e390edd25e0070f1f72c5354f9f4dcfc9359
Romit Jadhav <romitj1806@gmail.com>
2026-04-14 16:18:09 +0530

    Combined digit count in 1 function

--- Strivers Practice/Basics/Day_4/ArmstrongNumber.cpp
+++ Strivers Practice/Basics/Day_4/ArmstrongNumber.cpp
@@ -25,13 +25,32 @@ bool isArmstrong(int n){
 
 }
 
+bool isArmstrong2(int n) {
+        int num = 0;
+        int ld = 0;
+        int duplicate = n;
+        int digit_count = 0;
+         do {
+            duplicate = duplicate/10;
+            digit_count++;
+        } while (duplicate != 0);
+
+        duplicate = n;
+        while(duplicate != 0){
+            ld =  duplicate%10;
+            num = num + (int)pow(ld, digit_count);
+            duplicate = duplicate/10;
+        }
+        return num == n;
+
+    }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
     int x;
     cin >> x;
-    cout << isArmstrong(x);
+    cout << isArmstrong2(x);
     
 
     return 0;
