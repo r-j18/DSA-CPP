eb09891ccadbc418820a7d0c8223561b021542e9
Romit Jadhav <romitj1806@gmail.com>
2026-04-14 17:40:17 +0530

    Checked for prime numbers

---
+++ Strivers Practice/Basics/Day_4/PrimeCheck.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool isPrime(int n){
+    if(n <= 1 ) return false;
+
+    for(int i =2; i*i <= n ; i++){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+} 
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    cout << isPrime(n);
+    return 0;
+}
