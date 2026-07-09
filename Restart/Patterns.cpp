42c776e204f3e9059ff7eb253855884c5db79d2d
Romit Jadhav <romitj1806@gmail.com>
2026-07-09 21:47:18 +0530

    10 patterns done

--- Restart/Patterns.cpp
+++ Restart/Patterns.cpp
@@ -47,6 +47,88 @@ void pattern5(int n){
     }
 }
 
+void pattern6(int n){
+    for(int i = 1; i <=n; i++){
+        for(int j = 1; j<= n-i+1 ; j++){
+            cout << j;
+        }
+        cout << "\n";
+    }
+}
+
+void pattern7(int n){
+    for(int i =1; i <= n; i++){
+        for(int j = 1; j < n-i+1; j++){
+            cout << " ";
+        }
+        for(int k = 1; k <= 2*i-1 ; k++){
+            cout << "*";
+        }
+        for(int j = 1; j < n-i+1; j++){
+            cout << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void pattern8(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j =1; j <= i-1; j++ ){
+            cout << " ";
+        }
+        for(int k = 2*n - 1; k >= 2*i -1 ; k--){
+            cout << "*";
+        }
+        for(int j =1; j <= i-1; j++ ){
+            cout << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void pattern9(int n){
+    for(int i =1; i <= n; i++){
+        for(int j = 1; j < n-i+1; j++){
+            cout << " ";
+        }
+        for(int k = 1; k <= 2*i-1 ; k++){
+            cout << "*";
+        }
+        for(int j = 1; j < n-i+1; j++){
+            cout << " ";
+        }
+        cout << "\n";
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j =1; j <= i-1; j++ ){
+            cout << " ";
+        }
+        for(int k = 2*n - 1; k >= 2*i -1 ; k--){
+            cout << "*";
+        }
+        for(int j =1; j <= i-1; j++ ){
+            cout << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void pattern10(int n){
+    for(int i = 1; i<= 2*n-1; i++){
+        if(i <= n){
+            for(int j = 1; j<= i; j++){
+                cout << "*";
+            }
+            cout << "\n";
+        }
+        else{
+            for(int j = n-1; j >=i-n; j-- ){
+                cout << "*";
+            }
+            cout << "\n";
+        }
+    }
+}
 
 int main() {
 
@@ -60,7 +142,7 @@ int main() {
 
     int n;
     cin >> n;
-    pattern5(n);
+    pattern10(n);
 
     return 0;
 }
