9c5277e1afed5aedaa9905e212034346d7b7c008
Romit Jadhav <romitj1806@gmail.com>
2026-07-09 19:55:20 +0530

    2 patterns succesfull

--- Restart/Patterns.cpp
+++ Restart/Patterns.cpp
@@ -1,10 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-
-
 void pattern1(int n) {
     for(int i = 1; i <= n; i++) {
         for(int j =1; j <= n; j++){
@@ -15,6 +11,16 @@ void pattern1(int n) {
     
 }
 
+void pattern2(int n) {
+    for(int i = 1; i <=n; i++){
+        for(int j =1; j <= i; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+
+}
 
 
 
@@ -30,7 +36,7 @@ int main() {
 
     int n;
     cin >> n;
-    pattern1(n);
+    pattern2(n);
 
     return 0;
 }
