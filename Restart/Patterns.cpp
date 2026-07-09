730f0efe7201f32ac69dfdc4e944fb66bf524ab7
Romit Jadhav <romitj1806@gmail.com>
2026-07-09 19:40:14 +0530

    Setup and resolve

---
+++ Restart/Patterns.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+void pattern1(int n) {
+    for(int i = 1; i <= n; i++) {
+        for(int j =1; j <= n; j++){
+            cout << "*";
+        }
+        cout << "\n" ;
+    }
+    
+}
+
+
+
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    pattern1(n);
+
+    return 0;
+}
