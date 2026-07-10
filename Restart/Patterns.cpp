ceb264e07d3ff12dae24a31ca34a09f4c84de74c
Romit Jadhav <romitj1806@gmail.com>
2026-07-10 06:27:05 +0530

    All patterns done

--- Restart/Patterns.cpp
+++ Restart/Patterns.cpp
@@ -167,7 +167,54 @@ void pattern13(int n){
 }
 
 void pattern20(int n){
+    for(int i = 1; i<= 2*n-1; i++){
+        if(i <= n){
+            for(int j = 1; j<= i; j++){
+                cout << "*";
+            }
+            for(int j = 1; j <= 2*(n-i); j++){
+            cout << " ";
+            }
+            for(int j = 1; j<= i; j++){
+                cout << "*";
+            }   
+            cout << "\n";
+        }
+        else{
+            for(int j = n-1; j >=i-n; j-- ){
+                cout << "*";
+            }
+            for(int j = 1; j <= 2*(i-n); j++){
+            cout << " ";
+            }
+            for(int j = n-1; j >=i-n; j-- ){
+                cout << "*";
+            }
+            cout << "\n";
+        }
+    }
+}
+
+void pattern21(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++ ){
+        if(i == 1 || i == (n) || j == 1 || j == (n)) cout << "*";
+        else cout << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void pattern22(int n){
     
+    for(int i = 0; i < 2*n-1; i++){
+        for(int j = 0; j < 2*n-1; j++ ){
+                int dis[] = {i,j, (2*n - 2 - i), (2*n - 2 - j)};
+                int dpt = min(min(dis[0], dis[1]), min(dis[2], dis[3]));
+                cout << n-dpt;
+        }
+        cout << "\n";
+    }
 }
 
 int main() {
@@ -182,7 +229,7 @@ int main() {
 
     int n;
     cin >> n;
-    pattern13(n);
+    pattern22(n);
 
     return 0;
 }
